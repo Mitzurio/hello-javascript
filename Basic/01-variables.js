e3e772290fa40e62e6c560cf006377152d0b8fa5
Rocio <r.venegasrojas.06@gmail.com>
2025-07-09 17:50:28 -0400

    feat(lesson1): add hello world and variables examples

---
+++ Basic/01-variables.js
@@ -0,0 +1,22 @@
+// var (original way)
+
+var helloWorld = "Hello, JavaScript!"
+console.log(helloWorld)
+
+helloWorld = "Hello again, JavaScript!"
+console.log(helloWorld)
+
+// let (only accessible from a specific code block)
+
+let helloWorld2 = "Hello, JavaScript 2!"
+console.log(helloWorld2)
+
+// const (a constant...)
+
+const helloWorld3 = "Hello, JavaScript 3!"
+console.log(helloWorld3)
+
+// Error
+// const helloWorld3 = "Hello, JavaScript 3.1!"
+// console.log(helloWorld3)
+
